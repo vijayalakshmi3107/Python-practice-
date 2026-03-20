b29b34716d69742ed83122d306f6cc491bb4d994
vijayalakshmi3107 <vijayalakshmirs31@gmail.com>
2026-03-20 11:17:32 +0530

    Add temperature classification based on input value

---
+++ temperature.py
@@ -0,0 +1,13 @@
+temperature = int(input("Enter temperature: "))
+
+if temperature > 35:
+    print("Hot")
+
+elif temperature >= 20:
+    print("Warm")
+
+elif temperature >= 10:
+    print("Cool")
+
+else:
+    print("Cold")
